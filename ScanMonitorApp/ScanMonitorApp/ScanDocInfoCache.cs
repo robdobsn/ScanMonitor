27c3df4e50ca8a085401028e411ccc6eb8452398
Rob Dobson <rob__dobson@hotmail.com>
2014-05-20 13:05:57 +0100

    Cache improvements

---
+++ ScanMonitorApp/ScanMonitorApp/ScanDocInfoCache.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScanMonitorApp
+{
+    class ScanDocInfoCache
+    {
+        // Cache scan and filing info
+        private Dictionary<string, ScanDocAllInfo> _cacheScanDocAllInfo = new Dictionary<string, ScanDocAllInfo>();
+        private ScanDocHandler _scanDocHandler = null;
+
+        public ScanDocInfoCache(ScanDocHandler scanDocHandler)
+        {
+            _scanDocHandler = scanDocHandler;
+        }
+
+        public ScanDocAllInfo GetScanDocAllInfo(string uniqName)
+        {
+            ScanDocAllInfo sdAllInfo = null;
+
+            if (_cacheScanDocAllInfo.ContainsKey(uniqName))
+            {
+                sdAllInfo = _cacheScanDocAllInfo[uniqName];
+            }
+            else
+            {
+                sdAllInfo = _scanDocHandler.GetScanDocAllInfo(uniqName);
+                if (sdAllInfo != null)
+                    _cacheScanDocAllInfo.Add(uniqName, sdAllInfo);
+            }
+            return sdAllInfo;
+        }
+
+        public void UpdateDocInfo(string uniqName)
+        {
+            if (_cacheScanDocAllInfo.ContainsKey(uniqName))
+            {
+                ScanDocAllInfo sdAllInfo = _scanDocHandler.GetScanDocAllInfo(uniqName);
+                if (sdAllInfo != null)
+                    _cacheScanDocAllInfo[uniqName] = sdAllInfo;
+                else
+                    _cacheScanDocAllInfo.Remove(uniqName);
+            }
+            else
+            {
+                ScanDocAllInfo sdAllInfo = _scanDocHandler.GetScanDocAllInfo(uniqName);
+                if (sdAllInfo != null)
+                    _cacheScanDocAllInfo.Add(uniqName, sdAllInfo);
+            }
+        }
+    }
+
+}
